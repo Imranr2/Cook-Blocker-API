79ce112bf2a834916d43d924a0aad3aa565af404
Imranr2 <imranr2@msn.com>
2022-11-04 12:18:01 -0600

    Change relationship to 1 to N

--- internal/menuitem/ingredient.go
+++ internal/menuitem/ingredient.go
@@ -7,10 +7,7 @@ import (
 type Ingredient struct {
 	ID        uint        `json:"-" gorm:"primaryKey"`
 	Description string `json:"description" validate:"required" gorm:"not null"`
-	// Name      string      `json:"name" validate:"required" gorm:"not null"`
-	// Qty       uint        `json:"qty" validate:"required" gorm:"not null"`
-	// Unit      string      `json:"unit" validate:"required" gorm:"not null"`
-	MenuItems []*MenuItem `json:"-" gorm:"many2many:menu_item_ingredients"`
+	MenuItemID uint `json:"-" gorm:"not null"`
 	CreatedAt time.Time   `json:"-" gorm:"type:timestamp;default:current_timestamp"`
 	UpdatedAt time.Time   `json:"-" gorm:"type:timestamp;default:current_timestamp ON update current_timestamp"`
 }
